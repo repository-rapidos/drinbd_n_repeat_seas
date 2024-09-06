eb2779d2cebbf6f514f3a0d5c3d2d98739c37c46
repository-rapidos <136508595+repository-rapidos@users.noreply.github.com>
2024-09-06 10:41:55 +0200

    small change

--- all_in_one.py
+++ all_in_one.py
@@ -3151,7 +3151,7 @@ def manage(test_nbr,
 			if run_type == "realtime":
 				df_results_simul_realt_or_realtime_filename = df_results_realtime_filepath + f"df_results_realtime_model_test_nbr_{test_nbr}_run_id_{run_id}.csv"
 			elif run_type == "simulate_realtime":
-				df_results_simul_realt_or_realtime_filename = df_results_realtime_filepath + f"df_results_simul_realt_model_test_nbr_{test_nbr}_run_id_{run_id}.csv"
+				df_results_simul_realt_or_realtime_filename = df_results_simul_realt_filepath + f"df_results_simul_realt_model_test_nbr_{test_nbr}_run_id_{run_id}.csv"
 
 			df_results_2_save_realtime.to_csv(df_results_simul_realt_or_realtime_filename,
 											header = not check_file_exists(
